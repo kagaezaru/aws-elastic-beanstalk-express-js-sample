09cc05dca4d58b104a2de126eaac09cd01a7f475
norikoezaru <norikoezaru@gmail.com>
2022-03-18 14:36:28 +0900

    pipeline complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello My World From Local Working!'));
+app.get('/', (req, res) => res.send('Hello My World From Local Working with CI/CD!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
